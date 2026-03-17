dc37dc011738564903a4e7e59422edde1403dc66
Tarakesh <tkesh095@gmail.com>
2026-03-18 01:50:35 +0530

    fix: set API URL in all page components

--- frontend/src/pages/ResultsPage.js
+++ frontend/src/pages/ResultsPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 
-const API = '';
+const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 function SevBadge({ sev }) {
   const map = {
